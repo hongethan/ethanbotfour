8ad771d57dfbe9ca0db5c89fead3843abc231950
hongethan <614290813@qq.com>
2020-04-03 10:12:11 +0800

    commit test 2

--- bots/dispatchBot.js
+++ bots/dispatchBot.js
@@ -13,7 +13,7 @@ class DispatchBot extends ActivityHandler {
         const dispatchRecognizer = new LuisRecognizer({
             applicationId: process.env.LuisAppId,
             endpointKey: process.env.LuisAPIKey,
-            endpoint: 'https://${ process.env.LuisAPIHostName }'
+            endpoint: process.env.LuisAPIHostName
         }, {
             includeAllIntents: true,
             includeInstanceData: true
